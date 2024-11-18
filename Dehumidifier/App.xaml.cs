1463085090ba1a156df128c89477804cd7e8996a
Tark <connorjamessmith@gmail.com>
2024-11-18 23:02:53 +0000

    Add project files.

---
+++ Dehumidifier/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using WindowsFirewallHelper;
+
+namespace Dehumidifier;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
+{
+    private IFirewallRule rule;
+
+    public App()
+    {
+        rule = FirewallManager.Instance.CreateApplicationRule(
+            FirewallProfiles.Public | FirewallProfiles.Private | FirewallProfiles.Domain,
+            "SteamBlock",
+            FirewallAction.Block,
+            "C:\\Program Files (x86)\\Steam\\Steam.exe"
+        );
+        rule.Direction = FirewallDirection.Outbound;
+
+        NotifyIcon notifyIcon = new NotifyIcon();
+        notifyIcon.Icon = new Icon("icon.ico");
+        notifyIcon.Visible = true;
+        notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+        notifyIcon.ContextMenuStrip.Items.Add("Exit", null, (sender, e) => { Shutdown(); });
+    }
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        FirewallManager.Instance.Rules.Add(rule);
+    }
+
+    private void Application_Shutdown(object sender, ExitEventArgs e)
+    {
+        FirewallManager.Instance.Rules.Remove(rule);
+    }
+}
